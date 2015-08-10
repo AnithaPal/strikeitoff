037041033510fbfe620f5f008abd2ab347571b6d
AnithaPal <anitha.aravindvel@gmail.com>
2015-08-10 12:05:24 -0400

    user mangement feature test is in process

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,12 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
+require 'faker'
 
 FactoryGirl.define do
   factory :user do
+    name {Faker::Name.name}
+    email {Faker::Internet.email}
+    password 'helloworld'
+    password_confirmation 'helloworld'
+
   end
 end
