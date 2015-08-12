d6fca5b161839648390a725a1331100a1f48a290
AnithaPal <anitha.aravindvel@gmail.com>
2015-08-12 11:35:51 -0400

    add item feature added

---
+++ spec/features/todo_item_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "Todo items" do
+   before do
+      @user = create(:user)
+      visit 'users/sign_in'
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      click_button 'Sign in'
+      visit(user_path(@user))
+    end
+  
+  describe "Creating todo items" do
+   
+    it "allows user to create todo item" do
+      expect(page).to have_content "My Page"
+      find("#item_name").fill_in "New task", with: "New task to do"
+      # page.find_field("item_name").fill_in "New task", with: "New Task"
+      # fill_in "New_task", with: "New Task"
+    end
+  end
+
+  
