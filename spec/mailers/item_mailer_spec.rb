a68203ac5e70bedccf195f39cb2bddc1e2542c8f
AnithaPal <anitha.aravindvel@gmail.com>
2015-08-15 11:17:36 -0400

    mailer testing

--- spec/mailers/item_mailer_spec.rb
+++ spec/mailers/item_mailer_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe ItemMailer, :type => :mailer do
     let(:mail) { ItemMailer.new_item }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("New item")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.subject).to eq("New Todo Item is created")
+      expect(mail.to).to eq(["codetrain15@gmail.com"])
+      expect(mail.from).to eq(["admin@strikeitoff.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to match("A new todo item is created for you to finish:")
     end
   end
 
